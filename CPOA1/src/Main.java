16b0f0d54dbc1cc7c882f10e54b9c1c763203e99
Nathan.C <coursnathan57@gmail.com>
2021-09-13 14:54:21 +0200

    update fonctionnel

--- CPOA1/src/Main.java
+++ CPOA1/src/Main.java
@@ -2,9 +2,10 @@ import java.sql.*;
 
 public class Main {
 
-    public void uneRequete() {
+    /*public void uneRequete() {
         try {
-            Connection laConnexion = creeConnection();
+            Connexion connexion = new Connexion();
+            Connection laConnexion = connexion.creeConnexion();
             Statement requete = laConnexion.createStatement();
             ResultSet res = requete.executeQuery("select no_etudiant, nom_etudiant from etudiant");
 
@@ -12,12 +13,13 @@ public class Main {
                 int no = res.getInt(1);
                 String nom = res.getString("nom_etudiant");
             }
+
         } catch (SQLException sqle) {
             System.out.println("Pb select" + sqle.getMessage());
         }
-    }
-
+    }*/
     public static void main(String[] args) {
-
+        Periodicite petit = new Periodicite();
+        petit.update("test");
     }
 }
--- CPOA1/src/Periodicite.java
+++ CPOA1/src/Periodicite.java
@@ -2,4 +2,22 @@ import java.sql.*;
 
 public class Periodicite {
 
+    public void update(String prenom) {
+        try {
+            Connexion connexion = new Connexion();
+            Connection laConnexion = connexion.creeConnexion();
+
+            PreparedStatement req = laConnexion.prepareStatement("insert into Periodicite (libelle) values(?)",
+                            Statement.RETURN_GENERATED_KEYS);
+
+            req.setString(1, prenom);
+            int nbLignes = req.executeUpdate();
+            ResultSet res = req.getGeneratedKeys();
+            if (res.next()) {
+                int cle = res.getInt(1);
+            }
+        } catch (SQLException sqle) {
+            System.out.println("Pb dans select " + sqle.getMessage());
+        }
+    }
 }
