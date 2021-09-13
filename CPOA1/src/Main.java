e76ae9086ccdc6685496cbd46683dc17b5efb18b
Nathan.C <coursnathan57@gmail.com>
2021-09-13 16:42:48 +0200

    rajout de Client

--- CPOA1/src/Main.java
+++ CPOA1/src/Main.java
@@ -2,24 +2,9 @@ import java.sql.*;
 
 public class Main {
 
-    /*public void uneRequete() {
-        try {
-            Connexion connexion = new Connexion();
-            Connection laConnexion = connexion.creeConnexion();
-            Statement requete = laConnexion.createStatement();
-            ResultSet res = requete.executeQuery("select no_etudiant, nom_etudiant from etudiant");
-
-            while (res.next()) {
-                int no = res.getInt(1);
-                String nom = res.getString("nom_etudiant");
-            }
-
-        } catch (SQLException sqle) {
-            System.out.println("Pb select" + sqle.getMessage());
-        }
-    }*/
     public static void main(String[] args) {
-        Periodicite petit = new Periodicite();
-        petit.update("test");
+
+        Client cl1 = new Client();
+        cl1.update(1,"57890","COURS","2","France","Nathan","Diesen","2 impasse des ormes");
     }
 }
--- CPOA1/src/Periodicite.java
+++ CPOA1/src/Periodicite.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class Periodicite {
 
-    public void update(String prenom) {
+    public void insert (String libelle) {
         try {
             Connexion connexion = new Connexion();
             Connection laConnexion = connexion.creeConnexion();
@@ -10,8 +10,7 @@ public class Periodicite {
             PreparedStatement req = laConnexion.prepareStatement("insert into Periodicite (libelle) values(?)",
                             Statement.RETURN_GENERATED_KEYS);
 
-            req.setString(1, prenom);
-            int nbLignes = req.executeUpdate();
+            req.setString(1, libelle);
             ResultSet res = req.getGeneratedKeys();
             if (res.next()) {
                 int cle = res.getInt(1);
@@ -19,5 +18,37 @@ public class Periodicite {
         } catch (SQLException sqle) {
             System.out.println("Pb dans select " + sqle.getMessage());
         }
+    }
+    public void delete(int id){
+        try {
+            Connexion connexion = new Connexion();
+            Connection laConnexion = connexion.creeConnexion();
+
+            PreparedStatement requete = laConnexion.prepareStatement("delete from Periodicite where id_periodicite=?");
+            requete.setInt(1,id);
+            requete.executeUpdate();
+
+        }
+        catch (SQLException sqle){
+            System.out.println("Pb dans select " + sqle.getMessage());
+        }
+
+    }
+
+    public void update(int id,String lib){
+        try {
+            Connexion connexion = new Connexion();
+            Connection laConnexion = connexion.creeConnexion();
+
+            PreparedStatement requete = laConnexion.prepareStatement("update Periodicite SET libelle = ? WHERE id_periodicite = ?");
+            requete.setString(1,lib);
+            requete.setInt(2,id);
+            requete.executeUpdate();
+
+        }
+        catch (SQLException sqle){
+            System.out.println("Pb dans select " + sqle.getMessage());
+        }
+
     }
 }
