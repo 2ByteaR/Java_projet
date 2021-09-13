d3ab432e76a7ab0aca19518a0a0e4c5270e9ed27
Nathan.C <coursnathan57@gmail.com>
2021-09-13 11:05:02 +0200

    java td1

---
+++ CPOA1/src/Connexion.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+
+public class Connexion {
+
+    public Connection creeConnexion{
+        String url ="jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/cours3u_java";
+        String login = "etud1u_appli";
+
+    }
+}
