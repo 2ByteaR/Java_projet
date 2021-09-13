c2b0bd03d9ddffb351cb8e96ba62263749114596
Nathan.C <coursnathan57@gmail.com>
2021-09-13 16:57:17 +0200

    fini client/periodicite/connexion

--- CPOA1/src/Main.java
+++ CPOA1/src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         Client cl1 = new Client();
-        cl1.update(1,"57890","COURS","2","France","Nathan","Diesen","2 impasse des ormes");
+        cl1.delete(2);
     }
 }
